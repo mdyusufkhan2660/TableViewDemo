e0c25a0bb4d453ed97a625f5990c9457c48c7b8e
Md Yusuf Khan <md.yusuf.khan.2660@gmail.com>
2023-03-29 12:02:01 -0400

    Initial code
    In this project, I demonstrated how to create different design for custom cells in a table view

---
+++ CustomCellDesign/CustomCellDesign/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomCellDesign
+//
+//  Created by Md Yusuf Khan on 2023-03-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
